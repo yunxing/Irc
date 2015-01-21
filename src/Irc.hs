d69a5b0e3e652b361bdac201e214189ad01ca3b9
Yunxing Dai <yunxing@upthere.com>
2015-01-20 21:59:36 -0800

    Reformat

--- src/Irc.hs
+++ src/Irc.hs
@@ -1,16 +1,17 @@
 -- | Irc is a monadic DSL and library for writing Irc bots.
 --   It conveniently utilizes 'do' notation in the style of
---   <https://hackage.haskell.org/package/shake Shake> and
---   <https://hackage.haskell.org/package/clay clay>
+--   <https://hackage.haskell.org/package/shake Shake>
 --
 
 module Irc
     ( Behavior
+    , BehaviorM
     , Config(..)
     , mainWithConfigAndBehavior
     , (|!))
     where
 import Irc.Internal( Behavior
-    , Config(..)
-    , mainWithConfigAndBehavior
-    , (|!))
+                   , BehaviorM
+                   , Config(..)
+                   , mainWithConfigAndBehavior
+                   , (|!))
